4ea5b48838511a6a0f54bd5bb4c3db741ae9149d
Robert Plummer <robertleeplummerjr@gmail.com>
2020-09-10 13:05:08 -0400

    fix: Include generics in typings

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 import * as GPU from 'gpu.js';
 
 export function gpuMock(kernelFunction: GPU.KernelFunction, settings?: GPU.IKernelSettings): GPU.IKernelRunShortcut;
+export function gpuMock<T>(kernelFunction: GPU.KernelFunction<T>, settings?: GPU.IKernelSettings): GPU.IKernelRunShortcut;
